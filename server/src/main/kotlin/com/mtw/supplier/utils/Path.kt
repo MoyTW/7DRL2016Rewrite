bf42c2a2e2f9febd8296bf1dcc3f752dbab1b01a
MoyTW <moytravis@gmail.com>
2020-02-25 21:17:16 -0800

    LinePath

---
+++ server/src/main/kotlin/com/mtw/supplier/utils/Path.kt
@@ -0,0 +1,63 @@
+package com.mtw.supplier.utils
+
+import kotlinx.serialization.Serializable
+import kotlin.math.abs
+
+@Serializable
+class Path(val positions: List<XYCoordinates>) {
+    var _currentStep: Int = 0
+
+    fun currentPosition(): XYCoordinates {
+        return positions[_currentStep]
+    }
+
+    fun step(): XYCoordinates {
+        this._currentStep += 1
+        return currentPosition()
+    }
+
+    fun project(turns: Int) {
+        // TODO: Confirm this lol
+        positions.subList(_currentStep, _currentStep + turns)
+    }
+
+    fun atEnd(): Boolean {
+        return this._currentStep >= positions.size - 1
+    }
+}
+
+object PathBuilder {
+    /**
+     * Defines a straight-line path from (x0, y0) to (x1, y1), inclusive.
+     */
+    fun linePath(start: XYCoordinates, end: XYCoordinates): Path {
+        val acc = mutableListOf<XYCoordinates>()
+        val isVertical = start.x - end.x == 0
+        val dError: Float? = if (isVertical) { abs(end.y.toFloat() - start.y.toFloat()) / (end.x.toFloat() - start.x.toFloat()) } else null
+
+        val yErr = if (end.y - start.y > 0) 1 else -1
+        val xDiff = if (end.x - start.x > 0) 1 else -1
+
+        var error = 0F
+        var cX: Int = start.x
+        var cY: Int = start.y
+        val steps: Int = 0
+
+        while (!(cX == end.x && cY == end.y) && steps < 100) {
+            if (isVertical) {
+                cY += yErr
+                acc.add(XYCoordinates(cX, cY))
+            } else if (error >= 0.5F) {
+                cY += yErr
+                error -= 1F
+                acc.add(XYCoordinates(cX, cY))
+            } else {
+                cX += xDiff
+                error += dError!!
+                acc.add(XYCoordinates(cX, cY))
+            }
+        }
+        acc.add(XYCoordinates(cX, cY))
+        return Path(acc)
+    }
+}
