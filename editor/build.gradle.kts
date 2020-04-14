40f0a5c762f84551a70e65333c0947b8aff07eb0
MoyTW <moytravis@gmail.com>
2020-04-13 19:22:46 -0700

    Remove tornadoFx/javaFx

--- editor/build.gradle.kts
+++ editor/build.gradle.kts
@@ -1,27 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-/**
- * To set TornadoFX to run properly in IntelliJ:
- *
- * 1) Place the following line in the Run Configuration VM Options
- * --module-path "$JFX_SDK_DIR$\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.base,javafx.graphics
- *
- * Below...seems to work, somehow? without the JFX_SDK_DIR?
- * --module-path "$MODULE_DIR$\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.base,javafx.graphics --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED
- *
- * 2) Define $JFX_SDK_DIR$ in your env variables to the project dir /editor/javafx-sdk-11.0.2 (or whatever version)
- */
-
-
 plugins {
     kotlin("jvm")
     id("org.jetbrains.kotlin.plugin.serialization") version "1.3.50"
-    id("org.openjfx.javafxplugin") version "0.0.7"
-}
-
-javafx {
-    version = "12"
-    modules = listOf("javafx.controls", "javafx.base", "javafx.fxml", "javafx.graphics")
 }
 
 group = "com.mtw"
@@ -46,9 +27,6 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.50")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.13.0") // JVM dependency
-    implementation("no.tornado:tornadofx:1.7.17") {
-        exclude("org.jetbrains.kotlin")
-    }
     implementation("io.github.rybalkinsd:kohttp:0.11.1")
     implementation("org.hexworks.zircon:zircon.core-jvm:2020.1.1-PREVIEW")
     implementation("org.hexworks.zircon:zircon.jvm.swing:2020.1.1-PREVIEW")
